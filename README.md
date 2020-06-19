82dcb5bbb92d0519d7ad40cbda3b850c9a86d4a4
Christian <christiandp7@gmail.com>
2020-06-19 12:54:45 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Covid 19 Tracker Bolivia
+
+Covid 19 tracker, stadistics, probabilities, and proyections for Bolivia and Latam.
+
+# Installation
+
+> npm install
+
+# Run 
+
+> yarn start
+
+# build
+
+> yarn build
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+
+import MainLayout from "layouts/Main.js";
+
+import "assets/scss/black-dashboard-react.scss";
+import "assets/demo/demo.css";
+import "assets/css/nucleo-icons.css";
+
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    
+      <Route path="/"  render={props => <MainLayout {...props} />} />
+      {/*<Redirect from="/" to="/admin/dashboard" />*/}
+    
+  </Router>,
+  document.getElementById("root")
+);
