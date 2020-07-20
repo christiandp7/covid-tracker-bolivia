5765699b4b4e9efe3ff280cfea264cf678aeba0b
Christian <christiandp7@gmail.com>
2020-07-20 18:34:25 -0400

    adding _redirects and gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const gulp = require("gulp");
 const gap = require("gulp-append-prepend");
 
+gulp.task("redirects", async function() {
+  gulp.src('./_redirects')
+        .pipe(gulp.dest('./build/', { overwrite: true }));
+});
+
 gulp.task("licenses", async function() {
   // this is to add Creative Tim licenses in the production mode for the minified js
   gulp
