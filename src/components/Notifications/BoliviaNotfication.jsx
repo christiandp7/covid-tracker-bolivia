40482c7004e1f5f8cb90d20ca60d32b8dcacddf8
Christian <christiandp7@gmail.com>
2020-07-30 13:25:00 -0400

    change text notification

--- src/components/Notifications/BoliviaNotfication.jsx
+++ src/components/Notifications/BoliviaNotfication.jsx
@@ -19,7 +19,7 @@ function BoliviaNotfication() {
   return (
       <Alert color="primary" className="alert-with-icon" isOpen={visible} toggle={onDismiss}>
         <span className="tim-icons icon-bell-55" data-notify="icon" />
-        Los datos que se muestran corresponden al día 28/07/2020 publicados aproximadamente a las 10:30pm en agetic.gob.bo, desde esa fecha no se actualizaron los datos necesarios para el procesamiento de nuestra información, por ende estamos a la espera de la actualización de los datos su API. De momento pueden consultar los datos publicados el dia de ayer 29/07/2020 en <a href="https://www.boliviasegura.gob.bo/" target="_blank">boliviasegura.gob.bo</a>. En cuanto tengamos la actualización de los datos que necesitamos para nuestro procesamiento, se publicarán automaticamente.
+        Los datos que se muestran corresponden al día 28/07/2020 publicados aproximadamente a las 10:30pm en agetic.gob.bo, desde esa fecha no se actualizaron los datos que son necesarios para el procesamiento de nuestra información, por ende estamos a la espera de la actualización en su API. De momento pueden consultar los datos publicados el dia de ayer 29/07/2020 en <a href="https://www.boliviasegura.gob.bo/" target="_blank">boliviasegura.gob.bo</a>. En cuanto se efectue la actualización de los datos en agetic.gob.bo, se procesarán y posteriormente se publicarán automaticamente en nuestra página.
       </Alert>
   )
 }
